b47df1e96bddb3f8acbb492d9d7d891b59ca6c84
@enwerro <rock.enwerem@tecnotree.com>
2024-03-20 23:48:53 +0100

    Add slug migration

--- src/main/resources/db/migration/V3__add_slug_to_eventType.sql
+++ src/main/resources/db/migration/V3__add_slug_to_eventType.sql
@@ -1,4 +1,4 @@
-ALTER TABLE tbl_event_type ADD COLUMN slug NOT NULL  VARCHAR(255) UNIQUE, ADD CONSTRAINT unique_slug UNIQUE (slug);
+ALTER TABLE tbl_event_type ADD COLUMN slug  VARCHAR(255) NOT NULL UNIQUE;
 
 CREATE INDEX slug_index ON tbl_event_type (slug);
 
